b3a4fdea1723a5a99667ce3f256dcb4edda5ba59
ekmartin <mail@ekmartin.no>
2015-10-01 15:46:05 +0200

    Make it possible to add todos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class Header extends Component {
+  static propTypes = {
+    onSave: PropTypes.func.isRequired
+  }
+
+  state = {
+    text: ''
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    });
+  }
+
+  handleKeyDown(event) {
+    if (event.which === 13) {
+      const text = event.target.value.trim();
+      this.props.onSave(text);
+      this.setState({ text: '' });
+    }
+  }
+
   render() {
     return (
       <header>
-        <h1>Todos</h1>
+        <h1>Todo</h1>
         <input
           className='new-todo'
           placeholder='What needs to be done?'
-          autoFocus='true' />
+          autoFocus='true'
+          value={this.state.text}
+          onChange={this.handleChange.bind(this)}
+          onKeyDown={this.handleKeyDown.bind(this)} />
       </header>
     );
   }
