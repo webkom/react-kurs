3b0d1d7c757002fd2eb88ed06d7222a0bbe09e9e
ekmartin <mail@ekmartin.no>
2015-10-01 19:35:09 +0200

    Add state

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,23 @@ import Header from './Header';
 import Main from './Main';
 
 class App extends React.Component {
+  state = {
+    todos: [
+      {
+        text: 'Lær alt',
+        done: false
+      },
+      {
+        text: 'Vask gulvet!',
+        done: false
+      },
+      {
+        text: 'Spis pizza',
+        done: true
+      }
+    ]
+  }
+
   render() {
     return (
       <div className='todoapp'>
